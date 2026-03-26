4308c55f97c591deef540dbdc46db6513cd3e142
Kauã Felix <kfelixcosta11@gmail.com>
2026-03-26 14:16:20 -0300

    feat: inicia projeto

---
+++ wegoffice/src/main/java/com/centroweg/weg/wegoffice/WegofficeApplication.java
@@ -0,0 +1,13 @@
+package com.centroweg.weg.wegoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegofficeApplication.class, args);
+	}
+
+}
---
+++ wegoffice/src/test/java/com/centroweg/weg/wegoffice/WegofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroweg.weg.wegoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
