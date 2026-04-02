5c87b81e9d01b37ca87a756c81c2e0cb7c8ef1c4
Kauã Felix <kfelixcosta11@gmail.com>
2026-04-02 15:12:22 -0300

    feat: implement CRUD da entidade classes

--- wegoffice/src/main/java/com/centroweg/weg/wegoffice/infra/controller/ClassesController.java
+++ wegoffice/src/main/java/com/centroweg/weg/wegoffice/infra/controller/ClassesController.java
@@ -1,4 +1,60 @@
 package com.centroweg.weg.wegoffice.infra.controller;
 
+import com.centroweg.weg.wegoffice.application.dto.classes.ClassesRequestDto;
+import com.centroweg.weg.wegoffice.application.dto.classes.ClassesResponseDto;
+import com.centroweg.weg.wegoffice.application.service.ClassesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/classes")
 public class ClassesController {
+
+    private final ClassesService service;
+
+
+    @PostMapping
+    public ResponseEntity<ClassesResponseDto> save (@RequestBody ClassesRequestDto request){
+
+        ClassesResponseDto response = service.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClassesResponseDto> findById (@PathVariable Long id){
+
+        ClassesResponseDto response = service.findById(id);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClassesResponseDto>> findAll (){
+
+        List<ClassesResponseDto> response = service.findAll();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClassesResponseDto> update (@PathVariable Long id, @RequestBody ClassesRequestDto request){
+
+        ClassesResponseDto response = service.update(id, request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ClassesResponseDto> delete (@PathVariable Long id){
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
